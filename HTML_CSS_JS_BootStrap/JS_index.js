84967284c4ba07a207fcacc4cbb2451021967e76
adityaiiitr <adityaiiitr@gmail.com>
2022-05-21 18:20:09 +0530

    JavaScript Class 14

---
+++ HTML_CSS_JS_BootStrap/JS_index.js
@@ -0,0 +1,11 @@
+function abc() {
+    // This part is commented
+    /* This is Multiline Comments */
+
+    var a = "Hello I'm from Mars"
+    var b = "Hello I'm from Jupiter"
+    document.getElementById("myScriptData").innerHTML = a
+    document.getElementById("myScriptData1").innerHTML = b
+    const c = "hello"
+    console.log(c)
+}
